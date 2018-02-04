034a2eb6a464e9fae860cd9782c76fd66f76a328
Syang2016 <syang2016@chadwickschool.org>
2018-02-03 18:48:39 -0800

    llll

--- dwolla.js
+++ dwolla.js
@@ -4,20 +4,20 @@ var client = new dwolla.Client({id: "...", secret: "..."});
 
 var appToken = new client.Token({access_token: "GnlMMOlLLTTwRRC44gGPkjfsVbVtwzkptDVdIB0fwGwLnV481N"});
 
-var requestBody = {
-  _links: {
-    source: {
-      href: 'https://api-sandbox.dwolla.com/funding-sources/118b08b9-e1eb-48b7-94ad-866989b0764e'
-    },
-    destination: {
-      href: 'https://api-sandbox.dwolla.com/funding-sources/2fa64102-185d-443d-9001-dda9bc37651d'
-    }
-  },
-  amount: {
-    currency: 'USD',
-    value: '1.00'
-  }
-};
+// var requestBody = {
+//   _links: {
+//     source: {
+//       href: 'https://api-sandbox.dwolla.com/funding-sources/118b08b9-e1eb-48b7-94ad-866989b0764e'
+//     },
+//     destination: {
+//       href: 'https://api-sandbox.dwolla.com/funding-sources/2fa64102-185d-443d-9001-dda9bc37651d'
+//     }
+//   },
+//   amount: {
+//     currency: 'USD',
+//     value: '1.00'
+//   }
+// };
 
 appToken
   .post('transfers', requestBody)
--- src/pages/login/mpg.js
+++ src/pages/login/mpg.js
@@ -1,30 +1,18 @@
+const dwolla = require('dwolla-v2');
+
+// Navigate to https://www.dwolla.com/applications (production) or https://dashboard-sandbox.dwolla.com/applications (Sandbox) for your application key and secret.
+const appKey = "PkAsSdVJyZbUZSB7RkCd636M1CsgF6KKeZrt6ilq8xSMw9uMXc";
+const appSecret = "8Lo1LatM1cVAhEmpPEZ9Es0btxiZFF6PVAUl4xp14LTOsccHBl";
+const client = new dwolla.Client({
+  key: appKey,
+  secret: appSecret,
+  environment: 'sandbox' // optional - defaults to production
+});
+
+// create a token
+client.auth.client()
+
 export default function mpg() {
-    const dwolla = require('dwolla-v2');
-
-    const client = new dwolla.Client({id: "PkAsSdVJyZbUZSB7RkCd636M1CsgF6KKeZrt6ilq8xSMw9uMXc", secret: "8Lo1LatM1cVAhEmpPEZ9Es0btxiZFF6PVAUl4xp14LTOsccHBl", environment: 'sandbox'});
-
-    const appToken = new client.Token({access_token: "UvLP9NHzs4wX52l8mWsScUhuq5nOTLY3teVIkinzBRvbiVah8m"});
-
-    const requestBody = {
-        _links: {
-            source: {
-                href: 'https://api-sandbox.dwolla.com/funding-sources/118b08b9-e1eb-48b7-94ad-866989b0764e'
-            },
-            destination: {
-                href: 'https://api-sandbox.dwolla.com/funding-sources/2fa64102-185d-443d-9001-dda9bc37651d'
-            }
-        },
-        amount: {
-            currency: 'USD',
-            value: '1.00'
-        }
-    };
-
-    appToken
-        .post('transfers', requestBody)
-        .then(function(res) {
-            res.headers.get('location'); // => 'https://api-sandbox.dwolla.com/transfers/74c9129b-d14a-e511-80da-0aa34a9b2388'
-        });
 }
 
 
